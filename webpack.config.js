91a762e20504d2d1c2af114de891694951ab8692
v.karbovskyi <v.karbovskyi@ideil.com>
2019-02-26 15:01:42 +0200

    change library name

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'nj-select.js',
-        library: 'NJ-Select',
+        library: 'NJSelect',
         libraryTarget: 'umd'
     },
     module: {
