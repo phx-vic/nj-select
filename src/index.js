cb887849da44216b103950650b1ee513b9fc7edb
v.karbovskyi <v.karbovskyi@ideil.com>
2020-02-11 16:01:59 +0200

    add update, destroy

--- src/index.js
+++ src/index.js
@@ -4,10 +4,6 @@ export const NJSelect = class {
     constructor(el) {
         const self = this;
         self.select = el;
-        self.wrapper = document.createElement('div');
-        self.active = document.createElement('button');
-        self.list = document.createElement('ul');
-        self.activeIndex = self.select.selectedIndex;
         self.mobile = {
             Android: () => {
                 return navigator.userAgent.match(/Android/i);
@@ -34,53 +30,83 @@ export const NJSelect = class {
             open: 'nj--open',
             active: 'nj--active'
         };
+        self._init = () => {
+            self.wrapper = document.createElement('div');
+            self.active = document.createElement('button');
+            self.list = document.createElement('ul');
+            self.activeIndex = self.select.selectedIndex;
+            self.buttons = [];
+            self.wrapper.classList.add(self.className.main);
+            self.select.classList.add(`${self.className.main}__select`);
+            self.active.classList.add(`${self.className.main}__active`);
+            self.list.classList.add(`${self.className.main}__list`);
 
-        // scaffolding
-        self.wrapper.classList.add(self.className.main);
-        self.select.classList.add(`${self.className.main}__select`);
-        self.active.classList.add(`${self.className.main}__active`);
-        self.list.classList.add(`${self.className.main}__list`);
+            // scaffolding
+            self.select.parentNode.insertBefore(self.wrapper, self.select);
+            self.wrapper.appendChild(self.select);
+            self.wrapper.appendChild(self.active);
+            self.wrapper.appendChild(self.list);
 
-        self.select.parentNode.insertBefore(self.wrapper, self.select);
-        self.wrapper.appendChild(self.select);
-        self.wrapper.appendChild(self.active);
-        self.wrapper.appendChild(self.list);
+            for (let i = 0; i < self.select.options.length; i++) {
+                const item = self.select.options[i];
+                const li = document.createElement('li');
+                const btn = document.createElement('button');
+                const click = () => {
+                    self._setActive(i);
+                };
 
-        for (let i = 0; i < self.select.options.length; i++) {
-            const item = self.select.options[i];
-            const li = document.createElement('li');
-            const btn = document.createElement('button');
+                li.classList.add(`${self.className.main}__list-item`);
+                btn.classList.add(`${self.className.main}__list-btn`);
 
-            li.classList.add(`${self.className.main}__list-item`);
-            btn.classList.add(`${self.className.main}__list-btn`);
+                if (item.disabled) {
+                    btn.disabled = true;
+                }
 
-            if (item.disabled) {
-                btn.disabled = true;
+                self.list.appendChild(li);
+                li.appendChild(btn);
+                btn.innerHTML = item.innerHTML;
+
+                self.buttons.push(click);
+                btn.addEventListener('click', click);
             }
 
-            self.list.appendChild(li);
-            li.appendChild(btn);
-            btn.innerHTML = item.innerHTML;
+            // change active option
+            self._setActive();
 
-            btn.addEventListener('click', () => {
-                self.setActive(i);
-            });
-        }
+            // show/hide native select
+            if (self._isMobile()) {
+                self.wrapper.classList.add(self.className.native);
+            }
 
-        // change active option
-        self.setActive();
+            // events
+            self.select.addEventListener('change', self._selectChange);
+            self.active.addEventListener('click', self._activeClick);
+        };
+        self._setActive = index => {
+            const activeIndex = index >= 0 ? index : this.select.selectedIndex;
+            const items = this.list.children;
 
-        // show/hide native select
-        if (self.isMobile()) {
-            self.wrapper.classList.add(self.className.native);
-        }
+            // clear active class
+            items[this.activeIndex].classList.remove(this.className.active);
+            this.activeIndex = activeIndex;
 
-        // events
-        self.select.addEventListener('change', () => {
-            self.setActive();
-        });
+            // change selected option if clicked on list item
+            if (index >= 0) {
+                this.select.selectedIndex = this.activeIndex;
+                this.select.dispatchEvent(new Event('change'));
+            }
 
-        self.active.addEventListener('click', (event) => {
+            // set new active option
+            this.active.innerHTML = this.select.options[this.activeIndex].innerHTML;
+            items[this.activeIndex].classList.add(this.className.active);
+        };
+        self._isMobile = () => {
+            return this.mobile.any();
+        };
+        self._selectChange = () => {
+            self._setActive();
+        };
+        self._activeClick = (event) => {
             event.stopPropagation();
             self.wrapper.classList[self.wrapper.classList.contains(self.className.open) ? 'remove' : 'add'](self.className.open);
             document.body.addEventListener('click', close);
@@ -89,35 +115,31 @@ export const NJSelect = class {
                 self.wrapper.classList.remove(self.className.open);
                 document.body.removeEventListener('click', close);
             }
-        });
-    }
-
-    setActive(index) {
-        const activeIndex = index >= 0 ? index : this.select.selectedIndex;
-        const items = this.list.children;
-
-        // clear active class
-        items[this.activeIndex].classList.remove(this.className.active);
-        this.activeIndex = activeIndex;
-
-        // change selected option if clicked on list item
-        if (index >= 0) {
-            this.select.selectedIndex = this.activeIndex;
-            this.select.dispatchEvent(new Event('change'));
         }
 
-        // set new active option
-        this.active.innerHTML = this.select.options[this.activeIndex].innerHTML;
-        items[this.activeIndex].classList.add(this.className.active);
+        self._init();
     }
 
-    isMobile() {
-        return this.mobile.any();
+    update() {
+        this.destroy();
+        this._init();
     }
 
-    addEventListener(event, callback) {
-        this.select.addEventListener(event, callback);
-        return () => this.select.removeEventListener(event, callback);
+    destroy() {
+        const self = this;
+        const buttons = self.list.querySelectorAll(`.${self.className.main}__list-btn`);
+        for (let i = 0; i < buttons.length; i++) {
+            const btn = buttons[i];
+            btn.removeEventListener('click', self.buttons[i]);
+        }
+
+        self.select.removeEventListener('change', self._selectChange);
+        self.active.removeEventListener('click', self._activeClick);
+        self.wrapper.parentNode.insertBefore(self.select, self.wrapper);
+        self.select.classList.remove(`${self.className.main}__select`);
+        self.active.remove();
+        self.list.remove();
+        self.wrapper.remove();
     }
 }
 
